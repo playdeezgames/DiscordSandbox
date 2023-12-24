40143172aa6d182e31b6e783c33afe23df48d477
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-12-24 06:13:18 -0600

    const it up

--- SPLORR.Bot/SPLORRBot.vb
+++ SPLORR.Bot/SPLORRBot.vb
@@ -1,6 +1,10 @@
 ﻿Public Class SPLORRBot
     Implements IBot
 
+    Private Const TOKEN_STATUS As String = "status"
+    Private Const MESSAGE_NO_CHARACTER As String = "You have no character!"
+    Private Const MESSAGE_INVALID_INPUT As String = "Invalid input!"
+
     Public Sub Start() Implements IBot.Start
     End Sub
 
@@ -9,9 +13,9 @@
 
     Public Function HandleMessage(authorId As ULong, message As String) As String Implements IBot.HandleMessage
         message = message.ToLower
-        If message = "status" Then
-            Return "You have no character!"
+        If message = TOKEN_STATUS Then
+            Return MESSAGE_NO_CHARACTER
         End If
-        Return "Invalid input!"
+        Return MESSAGE_INVALID_INPUT
     End Function
 End Class
