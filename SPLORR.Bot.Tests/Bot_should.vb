eb1e9b796460928c8ca114eb5086352218a9e140
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-12-25 05:37:09 -0600

    rearrange and give better names to the particular tests

--- SPLORR.Bot.Tests/Bot_should.vb
+++ SPLORR.Bot.Tests/Bot_should.vb
@@ -30,12 +30,32 @@ Namespace SPLORR.Bot.Tests
         Const INVALID_INPUT_RESULT = "Invalid input!"
         <Theory>
         <InlineData(0, "", INVALID_INPUT_RESULT)>
+        Sub handle_empty_message_with_invalid_input(authorId As ULong, message As String, expectedMessage As String)
+            Dim worldModel As FakeWorldModel = New FakeWorldModel()
+            Dim subject As IBot = New SPLORRBot(worldModel)
+
+            Dim actual = subject.HandleMessage(authorId, message)
+            actual.ShouldBe(expectedMessage)
+
+            worldModel.FakePlayers.ShouldHaveSingleItem
+        End Sub
+        <Theory>
         <InlineData(0, "status", NO_CHARACTER_RESULT)>
         <InlineData(0, "Status", NO_CHARACTER_RESULT)>
         <InlineData(0, "statuS", NO_CHARACTER_RESULT)>
         <InlineData(0, "STATUS", NO_CHARACTER_RESULT)>
+        Sub handle_status_message_regardless_of_casing_when_player_has_no_character(authorId As ULong, message As String, expectedMessage As String)
+            Dim worldModel As FakeWorldModel = New FakeWorldModel()
+            Dim subject As IBot = New SPLORRBot(worldModel)
+
+            Dim actual = subject.HandleMessage(authorId, message)
+            actual.ShouldBe(expectedMessage)
+
+            worldModel.FakePlayers.ShouldHaveSingleItem
+        End Sub
+        <Theory>
         <InlineData(0, "status asdflkasdkfjal", INVALID_INPUT_RESULT)>
-        Sub handle_message(authorId As ULong, message As String, expectedMessage As String)
+        Sub handle_status_message_with_additional_tokens_with_invalid_input(authorId As ULong, message As String, expectedMessage As String)
             Dim worldModel As FakeWorldModel = New FakeWorldModel()
             Dim subject As IBot = New SPLORRBot(worldModel)
 
