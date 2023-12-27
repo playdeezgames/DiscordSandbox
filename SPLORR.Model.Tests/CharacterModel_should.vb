7ac91f8ac764cac171a8189f04d291e93adf8925
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-12-27 05:07:23 -0600

    character can set a name

--- SPLORR.Model.Tests/CharacterModel_should.vb
+++ SPLORR.Model.Tests/CharacterModel_should.vb
@@ -7,6 +7,14 @@ Public Class CharacterModel_should
         Dim subject = CreateSubject()
         subject.Name.ShouldNotBeNull
     End Sub
+    <Fact>
+    Sub allow_renaming()
+        Dim subject = CreateSubject()
+        Dim oldName = subject.Name
+        Dim newName = $"{oldName} Mc{oldName}face"
+        subject.Name = newName
+        subject.Name.ShouldBe(newName)
+    End Sub
 
     Private Shared Function CreateSubject() As ICharacterModel
         Const authorId As ULong = 0
