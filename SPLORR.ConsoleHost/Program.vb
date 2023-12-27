5a1eb104d2f17a64777d4c91b758ad3a736ec6d7
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-12-27 06:07:34 -0600

    the console host gives me good information about the exception thrown

--- SPLORR.ConsoleHost/Program.vb
+++ SPLORR.ConsoleHost/Program.vb
@@ -8,14 +8,20 @@ Module Program
     Private bot As IBot
     Sub Main(args As String())
         AddHandler Console.CancelKeyPress, AddressOf OnCancelKeyPress
-        Dim worldModel As New WorldModel(New DataStore(Environment.GetEnvironmentVariable(ENV_VAR_CONNECTION_STRING)))
-        bot = New SPLORRBot(worldModel)
-        bot.Start()
-        Do
-            Console.Write("> ")
-            Dim input = Console.ReadLine
-            Console.WriteLine(bot.HandleMessage(0, input))
-        Loop
+        Try
+            Dim worldModel As New WorldModel(New DataStore(Environment.GetEnvironmentVariable(ENV_VAR_CONNECTION_STRING)))
+            bot = New SPLORRBot(worldModel)
+            bot.Start()
+            Do
+                Console.Write("> ")
+                Dim input = Console.ReadLine
+                Console.WriteLine(bot.HandleMessage(0, input))
+            Loop
+        Catch ex As Exception
+            bot.Stop()
+            Console.WriteLine($"Exception: {ex}")
+            Console.ReadLine()
+        End Try
     End Sub
 
     Private Sub OnCancelKeyPress(sender As Object, e As ConsoleCancelEventArgs)
