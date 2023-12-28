a91d5d23ca2b7ec09866af8d2061288ea120cb8f
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-12-28 04:34:22 -0600

    added player character types and character types to the database

---
+++ SPLORR.Database/dbo/Tables/CharacterTypes.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[CharacterTypes]
+(
+	[CharacterTypeId] INT NOT NULL IDENTITY(1,1),
+	[CharacterTypeName] NVARCHAR(100) NOT NULL,
+	CONSTRAINT PK_CharacterTypes PRIMARY KEY([CharacterTypeId])
+)
