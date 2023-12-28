26ab5e4365c418ab34d3f506c242b5fc6e769717
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-12-28 05:18:28 -0600

    i can add a character to the database through commands

--- SPLORR.Data/DataStore.vb
+++ SPLORR.Data/DataStore.vb
@@ -2,8 +2,27 @@
 
 Public Class DataStore
     Implements IDataStore
-    Const TABLE_PLAYERS = "Players"
+    Const FIELD_CHARACTER_ID = "CharacterId"
+    Const FIELD_CHARACTER_NAME = "CharacterName"
+    Const FIELD_CHARACTER_TYPE_ID = "CharacterTypeId"
+    Const FIELD_DISCORD_ID = "DiscordId"
+    Const FIELD_GENERATOR_WEIGHT = "GeneratorWeight"
+    Const FIELD_LOCATION_ID = "LocationId"
     Const FIELD_PLAYER_ID = "PlayerId"
+
+    Const PARAMETER_CHARACTER_ID = "@CharacterId"
+    Const PARAMETER_DISCORD_ID = "@DiscordId"
+    Const PARAMETER_PLAYER_ID = "@PlayerId"
+    Const PARAMETER_CHARACTER_NAME = "@CharacterName"
+    Const PARAMETER_LOCATION_ID = "@LocationId"
+    Const PARAMETER_CHARACTER_TYPE_ID = "@CharacterTypeId"
+
+    Const TABLE_CHARACTERS = "Characters"
+    Const TABLE_LOCATION_STARTS = "LocationStarts"
+    Const TABLE_PLAYERS = "Players"
+    Const TABLE_PLAYER_CHARACTER_TYPES = "PlayerCharacterTypes"
+    Const TABLE_PLAYER_CHARACTERS = "PlayerCharacters"
+
     Private _connectionString As String
     Private _connection As SqlConnection = Nothing
     Private Function GetConnection() As SqlConnection
@@ -18,17 +37,64 @@ Public Class DataStore
     End Sub
 
     Public Sub CreatePlayerCharacter(playerId As Integer, characterName As String, locationId As Integer, characterType As Integer) Implements IDataStore.CreatePlayerCharacter
-        Throw New NotImplementedException()
+        Dim characterId As Integer = CreateCharacter(characterName, locationId, characterType)
+        SetPlayerCharacter(playerId, characterId)
     End Sub
 
+    Private Sub SetPlayerCharacter(playerId As Integer, characterId As Integer)
+        Using command = GetConnection().CreateCommand
+            command.CommandText = $"
+INSERT INTO 
+    {TABLE_PLAYER_CHARACTERS}
+    (
+        {FIELD_PLAYER_ID},
+        {FIELD_CHARACTER_ID}
+    ) 
+    VALUES 
+    (
+        {PARAMETER_PLAYER_ID},
+        {PARAMETER_CHARACTER_ID}
+    );"
+            command.Parameters.AddWithValue(PARAMETER_CHARACTER_ID, characterId)
+            command.Parameters.AddWithValue(PARAMETER_PLAYER_ID, playerId)
+            command.ExecuteNonQuery()
+        End Using
+    End Sub
+
+    Private Function CreateCharacter(characterName As String, locationId As Integer, characterType As Integer) As Integer
+        Using command = GetConnection().CreateCommand
+            command.CommandText = $"
+INSERT INTO 
+    {TABLE_CHARACTERS}
+    (
+        {FIELD_CHARACTER_NAME},
+        {FIELD_LOCATION_ID},
+        {FIELD_CHARACTER_TYPE_ID}
+    ) 
+    VALUES 
+    (
+        {PARAMETER_CHARACTER_NAME},
+        {PARAMETER_LOCATION_ID},
+        {PARAMETER_CHARACTER_TYPE_ID}
+    );"
+            command.Parameters.AddWithValue(PARAMETER_CHARACTER_NAME, characterName)
+            command.Parameters.AddWithValue(PARAMETER_LOCATION_ID, locationId)
+            command.Parameters.AddWithValue(PARAMETER_CHARACTER_TYPE_ID, characterType)
+            command.ExecuteNonQuery()
+        End Using
+        Using command = GetConnection().CreateCommand
+            command.CommandText = $"SELECT @@IDENTITY;"
+            Return CInt(command.ExecuteScalar)
+        End Using
+    End Function
+
     Public Function CheckForCharacter(playerId As Integer) As Boolean Implements IDataStore.CheckForCharacter
-        Const PARAMETER_PLAYER_ID = "@PlayerId"
         Using command = GetConnection().CreateCommand
             command.CommandText = $"
 SELECT 
     COUNT(1) 
 FROM 
-    {TABLE_PLAYERS} 
+    {TABLE_PLAYER_CHARACTERS} 
 WHERE 
     {FIELD_PLAYER_ID}={PARAMETER_PLAYER_ID};"
             command.Parameters.AddWithValue(PARAMETER_PLAYER_ID, playerId)
@@ -38,8 +104,6 @@ WHERE
 
     Public Function GetPlayerForAuthor(authorId As ULong) As Integer Implements IDataStore.GetPlayerForAuthor
         Dim discordId = CLng(authorId)
-        Const PARAMETER_DISCORD_ID = "@DiscordId"
-        Const FIELD_DISCORD_ID = "DiscordId"
         Dim playerId As Integer? = Nothing
         Using command = GetConnection().CreateCommand()
             command.CommandText = $"
@@ -84,10 +148,38 @@ VALUES
     End Sub
 
     Public Function GetCharacterTypeGenerator() As IReadOnlyDictionary(Of Integer, Integer) Implements IDataStore.GetCharacterTypeGenerator
-        Throw New NotImplementedException()
+        Dim result As New Dictionary(Of Integer, Integer)
+        Using command = GetConnection().CreateCommand
+            command.CommandText = $"
+SELECT 
+    {FIELD_CHARACTER_TYPE_ID}, 
+    {FIELD_GENERATOR_WEIGHT} 
+FROM 
+    {TABLE_PLAYER_CHARACTER_TYPES};"
+            Using reader = command.ExecuteReader
+                While reader.Read
+                    result(reader.GetInt32(0)) = reader.GetInt32(1)
+                End While
+            End Using
+        End Using
+        Return result
     End Function
 
     Public Function GetLocationGenerator() As IReadOnlyDictionary(Of Integer, Integer) Implements IDataStore.GetLocationGenerator
-        Throw New NotImplementedException()
+        Dim result As New Dictionary(Of Integer, Integer)
+        Using command = GetConnection().CreateCommand
+            command.CommandText = $"
+SELECT 
+    {FIELD_LOCATION_ID}, 
+    {FIELD_GENERATOR_WEIGHT} 
+FROM 
+    {TABLE_LOCATION_STARTS};"
+            Using reader = command.ExecuteReader
+                While reader.Read
+                    result(reader.GetInt32(0)) = reader.GetInt32(1)
+                End While
+            End Using
+        End Using
+        Return result
     End Function
 End Class
--- SPLORR.Database/dbo/Tables/Characters.sql
+++ SPLORR.Database/dbo/Tables/Characters.sql
@@ -2,8 +2,9 @@
     [CharacterId]   INT            IDENTITY (1, 1) NOT NULL,
     [CharacterName] NVARCHAR (100) NOT NULL,
     [LocationId]    INT            NOT NULL,
-    [CharacterType] INT            NOT NULL,
+    [CharacterTypeId] INT            NOT NULL,
     CONSTRAINT [PK_CharacterId] PRIMARY KEY CLUSTERED ([CharacterId] ASC),
-    CONSTRAINT [FK_Characters_Locations] FOREIGN KEY ([LocationId]) REFERENCES [dbo].[Locations] ([LocationId])
+    CONSTRAINT [FK_Characters_Locations] FOREIGN KEY ([LocationId]) REFERENCES [dbo].[Locations] ([LocationId]),
+    CONSTRAINT [FK_Characters_CharacterTypes] FOREIGN KEY ([CharacterTypeId]) REFERENCES [dbo].[CharacterTypes]([CharacterTypeId])
 );
 
