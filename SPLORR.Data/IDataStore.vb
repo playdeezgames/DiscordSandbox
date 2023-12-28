6866c11f472464c3dc4788e55d33fbf14f6fdaa8
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-12-28 06:00:19 -0600

    some notes in idatastore for how to refactor into substores

--- SPLORR.Data/IDataStore.vb
+++ SPLORR.Data/IDataStore.vb
@@ -1,11 +1,15 @@
 Public Interface IDataStore
+    'boilerplate
+    Sub CleanUp()
+    'PlayerStore
     Sub CreatePlayerCharacter(playerId As Integer, characterName As String, locationId As Integer, characterType As Integer)
     Function CheckForCharacter(playerId As Integer) As Boolean
+    Function GetCharacterForPlayer(playerId As Integer) As Integer
+    '???
     Function GetPlayerForAuthor(authorId As ULong) As Integer
-    Sub CleanUp()
     Function GetCharacterTypeGenerator() As IReadOnlyDictionary(Of Integer, Integer)
     Function GetLocationGenerator() As IReadOnlyDictionary(Of Integer, Integer)
-    Function GetCharacterForPlayer(playerId As Integer) As Integer
+    'CharacterStore
     Function GetCharacterName(characterId As Integer) As String
     Sub SetCharacterName(characterId As Integer, characterName As String)
 End Interface
