cc3d9df1349f9ec706c1e4565ea593855dcda31f
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-12-30 07:30:39 -0600

    location type verb types

---
+++ SPLORR.Database/dbo/Tables/LocationTypeVerbTypes.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[LocationTypeVerbTypes]
+(
+	[LocationTypeVerbTypeId] INT NOT NULL IDENTITY(1,1),
+	[LocationTypeId] INT NOT NULL,
+	[VerbTypeId] INT NOT NULL,
+	CONSTRAINT PK_LocationTypeVerbTypes PRIMARY KEY([LocationTypeVerbTypeId]),
+	CONSTRAINT FK_LocationTypeVerbTypes_LocationTypes FOREIGN KEY ([LocationTypeId]) REFERENCES LocationTypes(LocationTypeId),
+	CONSTRAINT FK_LocationTypeVerbTypes_VerbTypes FOREIGN KEY ([VerbTypeId]) REFERENCES VerbTypes(VerbTypeId),
+	CONSTRAINT AK_LocationType_VerbTypes_LocationTypeId_VerbTypeId UNIQUE(LocationTypeId, VerbTypeId)
+)
---
+++ SPLORR.Database/dbo/Tables/VerbTypes.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[VerbTypes]
+(
+	[VerbTypeId] INT NOT NULL IDENTITY(1,1),
+	[VerbTypeName] NVARCHAR(100) NOT NULL,
+	CONSTRAINT PK_VerbTypes PRIMARY KEY([VerbTypeId]),
+	CONSTRAINT AK_VerbTypes_VerbTypeName UNIQUE([VerbTypeName])
+)
