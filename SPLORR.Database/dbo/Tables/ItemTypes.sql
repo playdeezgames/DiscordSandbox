b8064b891704162cc67d634d8df5ee67bdda945d
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-01-03 04:04:58 -0600

    added item types to database

---
+++ SPLORR.Database/dbo/Tables/ItemTypes.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ItemTypes]
+(
+	[ItemTypeId] INT NOT NULL IDENTITY(1,1),
+	[ItemTypeName] NVARCHAR(100) NOT NULL,
+	CONSTRAINT PK_ItemTypes PRIMARY KEY(ItemTypeId),
+	CONSTRAINT AK_ItemTypes_ItemTypeName UNIQUE(ItemTypeName)
+)
