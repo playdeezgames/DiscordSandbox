81dd162696446f829acf79adc6b367a6465bbba4
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-01-03 05:58:10 -0600

    the item type generator and related to item type tables

---
+++ SPLORR.Database/dbo/Tables/ItemTypeGenerators.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ItemTypeGenerators]
+(
+	[ItemTypeGeneratorId] INT NOT NULL IDENTITY(1,1),
+	[ItemTypeGeneratorName] NVARCHAR(100) NOT NULL,
+	CONSTRAINT PK_ItemTypeGenerators PRIMARY KEY(ItemTypeGeneratorId),
+	CONSTRAINT AK_ItemTypeGenerators_ItemTypeGeneratorName UNIQUE(ItemTypeGeneratorName)
+)
