9f80ce78ecaf742539de4fca4e246a0eee2fd0af
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-01-03 05:35:07 -0600

    base add type works

--- SPLORR.Editor/ItemType/ItemTypeAddWindow.vb
+++ SPLORR.Editor/ItemType/ItemTypeAddWindow.vb
@@ -1,56 +1,11 @@
-﻿Imports SPLORR.Data
-Imports Terminal.Gui
-
-Friend Class ItemTypeAddWindow
-    Inherits Window
-
-    Private ReadOnly dataStore As Data.IDataStore
-    Private nameTextField As TextField
-
+﻿Friend Class ItemTypeAddWindow
+    Inherits BaseAddTypeWindow
     Public Sub New(dataStore As Data.IDataStore)
-        MyBase.New("Add Item Type...")
-        Me.dataStore = dataStore
-        Dim nameLabel As New Label("Name:") With
-            {
-                .X = 1,
-                .Y = 1
-            }
-        nameTextField = New TextField() With
-            {
-                .X = Pos.Right(nameLabel) + 1,
-                .Y = nameLabel.Y,
-                .Width = [Dim].Fill - 1
-            }
-        Dim addButton As New Button("Add") With
-            {
-                .X = 1,
-                .Y = Pos.Bottom(nameLabel) + 1
-            }
-        AddHandler addButton.Clicked, AddressOf OnAddButtonClicked
-        Dim cancelButton As New Button("Cancel") With
-            {
-                .X = Pos.Right(addButton) + 1,
-                .Y = addButton.Y
-            }
-        AddHandler cancelButton.Clicked, AddressOf OnCancelButtonClicked
-        Add(nameLabel, nameTextField, addButton, cancelButton)
-    End Sub
-
-    Private Sub OnCancelButtonClicked()
-        GoToWindow(New ItemTypeListWindow(dataStore))
-    End Sub
-
-    Private Sub OnAddButtonClicked()
-        Dim itemTypeName = nameTextField.Text.ToString
-        If String.IsNullOrEmpty(itemTypeName) Then
-            MessageBox.ErrorQuery("Error!", "Item Type must have a name!", "Ok")
-            Return
-        End If
-        If dataStore.ItemTypeNameExists(itemTypeName) Then
-            MessageBox.ErrorQuery("Duplicate!", "Item Type Name must be unique!", "Ok")
-            Return
-        End If
-        Dim itemType As IItemTypeStore = dataStore.CreateItemType(itemTypeName)
-        GoToWindow(New ItemTypeListWindow(dataStore))
+        MyBase.New(
+            "Add Item Type...",
+            "Item Type",
+            Function(x) dataStore.ItemTypeNameExists(x),
+            Function() New ItemTypeListWindow(dataStore),
+            Function(x) New ItemTypeEditWindow(dataStore.CreateItemType(x)))
     End Sub
 End Class
--- SPLORR.Editor/LocationType/Crud/LocationTypeAddWindow.vb
+++ SPLORR.Editor/LocationType/Crud/LocationTypeAddWindow.vb
@@ -1,55 +1,13 @@
 ﻿Imports Terminal.Gui
 
 Friend Class LocationTypeAddWindow
-    Inherits Window
-
-    Private ReadOnly dataStore As Data.IDataStore
-    Private nameTextField As TextField
-
+    Inherits BaseAddTypeWindow
     Public Sub New(dataStore As Data.IDataStore)
-        MyBase.New("Add Location Type...")
-        Me.dataStore = dataStore
-        Dim nameLabel As New Label("Name:") With
-            {
-                .X = 1,
-                .Y = 1
-            }
-        nameTextField = New TextField() With
-            {
-                .X = Pos.Right(nameLabel) + 1,
-                .Y = nameLabel.Y,
-                .Width = [Dim].Fill - 1
-            }
-        Dim addButton As New Button("Add") With
-            {
-                .X = 1,
-                .Y = Pos.Bottom(nameLabel) + 1
-            }
-        AddHandler addButton.Clicked, AddressOf OnAddButtonClicked
-        Dim cancelButton As New Button("Cancel") With
-            {
-                .X = Pos.Right(addButton) + 1,
-                .Y = addButton.Y
-            }
-        AddHandler cancelButton.Clicked, AddressOf OnCancelButtonClicked
-        Add(nameLabel, nameTextField, addButton, cancelButton)
-    End Sub
-
-    Private Sub OnCancelButtonClicked()
-        Program.GoToWindow(New LocationTypeListWindow(dataStore))
-    End Sub
-
-    Private Sub OnAddButtonClicked()
-        Dim locationTypeName = nameTextField.Text.ToString
-        If String.IsNullOrEmpty(locationTypeName) Then
-            MessageBox.ErrorQuery("Error!", "Location Type must have a name!", "Ok")
-            Return
-        End If
-        If dataStore.LocationTypeNameExists(locationTypeName) Then
-            MessageBox.ErrorQuery("Duplicate!", "Location Type Name must be unique!", "Ok")
-            Return
-        End If
-        dataStore.CreateLocationType(locationTypeName)
-        Program.GoToWindow(New LocationTypeListWindow(dataStore))
+        MyBase.New(
+            "Add Location Type...",
+            "Location Type",
+            Function(x) dataStore.LocationTypeNameExists(x),
+            Function() New LocationTypeListWindow(dataStore),
+            Function(x) New LocationTypeEditWindow(dataStore.CreateLocationType(x)))
     End Sub
 End Class
