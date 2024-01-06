a9fcd15d5c637f76d1b3e019a00cd595435168db
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-01-06 03:53:18 -0600

    added inventories and items

---
+++ SPLORR.Database/dbo/Tables/Item/Inventories.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Inventories]
+(
+	[InventoryId] INT NOT NULL IDENTITY(1,1),
+	[CharacterId] INT NULL,
+	[LocationId] INT NULL,
+	CONSTRAINT PK_Inventories PRIMARY KEY(InventoryId),
+	CONSTRAINT AK_Inventories_CharacterId UNIQUE(CharacterId),
+	CONSTRAINT AK_Inventories_LocationId UNIQUE(LocationId),
+	CONSTRAINT FK_Inventories_Characters FOREIGN KEY (CharacterId) REFERENCES Characters(CharacterId),
+	CONSTRAINT FK_Inventories_Locations FOREIGN KEY (LocationId) REFERENCES Locations(LocationId),
+	CONSTRAINT CK_Inventoreis_CharacterId_LocationId CHECK((CharacterId IS NULL AND LocationId IS NOT NULL) OR (CharacterId IS NOT NULL AND LocationId IS NULL))
+
+)
---
+++ SPLORR.Database/dbo/Tables/Item/Items.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Items]
+(
+	[ItemId] INT NOT NULL IDENTITY(1,1),
+	[InventoryId] INT NOT NULL,
+	[ItemTypeId] INT NOT NULL,
+	CONSTRAINT PK_Items PRIMARY KEY(ItemId),
+	CONSTRAINT FK_Items_Inventories FOREIGN KEY(InventoryId) REFERENCES Inventories(InventoryId),
+	CONSTRAINT FK_Items_ItemTypes FOREIGN KEY(ItemTypeId) REFERENCES ItemTypes(ItemTypeId)
+)
