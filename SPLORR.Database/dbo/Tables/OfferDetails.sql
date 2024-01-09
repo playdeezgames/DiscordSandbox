d6d9c3163ad7a093fbf97c7793528b36159eb841
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-01-09 05:55:33 -0600

    offers and offer details table

---
+++ SPLORR.Database/dbo/Tables/OfferDetails.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[OfferDetails]
+(
+	[OfferDetailId] INT NOT NULL IDENTITY(1,1),
+	[OfferId] INT NOT NULL,
+	[ItemTypeId] INT NOT NULL,
+	[QuantityIn] INT NOT NULL,
+	[QuantityOut] INT NOT NULL,
+	CONSTRAINT PK_OfferDetails PRIMARY KEY(OfferDetailId),
+	CONSTRAINT FK_OfferDetails_Offers FOREIGN KEY (OfferId) REFERENCES Offers(OfferId),
+	CONSTRAINT FK_OfferDetails_ItemTypes FOREIGN KEY (ItemTypeId) REFERENCES ItemTypes(ItemTypeId),
+	CONSTRAINT CK_OfferDetails_QuantityIn CHECK(QuantityIn>=0),
+	CONSTRAINT CK_OfferDetails_QuantityOut CHECK(QuantityOut>=0),
+	CONSTRAINT CK_OfferDetails_QuantityIn_QuantityOut CHECK(QuantityIn+QuantityOut>0),
+	CONSTRAINT AK_OfferDetails_OfferId_ItemTypeId UNIQUE(OfferId, ItemTypeId)
+)
