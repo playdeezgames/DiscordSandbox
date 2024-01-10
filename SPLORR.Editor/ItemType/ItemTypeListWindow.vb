c8fde9de8f28567bcc184b2f850d93ebed2ee3f7
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-01-10 04:22:08 -0600

    normalized ItemTypeListWindow parameter names

--- SPLORR.Editor/ItemType/ItemTypeListWindow.vb
+++ SPLORR.Editor/ItemType/ItemTypeListWindow.vb
@@ -3,16 +3,16 @@
 Friend Class ItemTypeListWindow
     Inherits BaseListWindow(Of IDataStore, IItemTypeStore)
 
-    Public Sub New(dataStore As Data.IDataStore)
+    Public Sub New(store As Data.IDataStore)
         MyBase.New(
             "Item Types",
-            dataStore,
-            Function(store, filter) store.ItemTypes.Filter(filter),
-            Function(item) New ItemTypeListItem(item),
-            Function(item) New ItemTypeEditWindow(CType(item, ItemTypeListItem).Store),
+            store,
+            Function(x, y) x.ItemTypes.Filter(y),
+            Function(x) New ItemTypeListItem(x),
+            Function(x) New ItemTypeEditWindow(CType(x, ItemTypeListItem).Store),
             AdditionalButtons:=
             {
-                ("Add...", Function() True, Sub() Program.GoToWindow(New ItemTypeAddWindow(dataStore))),
+                ("Add...", Function() True, Sub() Program.GoToWindow(New ItemTypeAddWindow(store))),
                 ("Close", Function() True, Sub() Program.GoToWindow(Nothing))
             })
     End Sub
