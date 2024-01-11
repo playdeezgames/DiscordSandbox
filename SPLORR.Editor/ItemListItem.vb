0bb943e81c29f0e362b9ceafe74bb16a14983e04
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-01-11 03:45:41 -0600

    i can see inventories of character and locations?

---
+++ SPLORR.Editor/ItemListItem.vb
@@ -0,0 +1,13 @@
+﻿Imports SPLORR.Data
+
+Friend Class ItemListItem
+    Private ReadOnly Property Store As IItemStore
+
+    Public Sub New(x As IItemStore)
+        Me.Store = x
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return $"{Store.Name}(Id:{Store.Id})"
+    End Function
+End Class
