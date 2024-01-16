ac295e5bba83920bffb86c8ed2070c0a46a1b513
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-01-16 12:07:04 -0600

    PARAMETER_INVENTORY_ID

--- SPLORR.Data/Grimoire/Parameters.vb
+++ SPLORR.Data/Grimoire/Parameters.vb
@@ -6,5 +6,4 @@
     Friend Const PARAMETER_CHARACTER_NAME = "@CharacterName"
     Friend Const PARAMETER_CHARACTER_TYPE_ID = "@CharacterTypeId"
     Friend Const PARAMETER_GENERATOR_WEIGHT = "@GeneratorWeight"
-    Friend Const PARAMETER_INVENTORY_ID = "@InventoryId"
 End Module
