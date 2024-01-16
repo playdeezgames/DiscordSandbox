e92ef83a168c4ba48a03f63db083bc4b1e87369e
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-01-16 12:07:43 -0600

    PARAMETER_CHARACTER_TYPE_ID

--- SPLORR.Data/Grimoire/Parameters.vb
+++ SPLORR.Data/Grimoire/Parameters.vb
@@ -4,5 +4,4 @@
     Friend Const PARAMETER_DIRECTION_NAME = "@DirectionName"
     Friend Const PARAMETER_DISCORD_ID = "@DiscordId"
     Friend Const PARAMETER_CHARACTER_NAME = "@CharacterName"
-    Friend Const PARAMETER_CHARACTER_TYPE_ID = "@CharacterTypeId"
 End Module
