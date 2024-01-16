c08fa3b577b47d81cd6aadea3baf6c99b5949a7e
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-01-16 12:12:30 -0600

    PARAMETER_DIRECTION_NAME

--- SPLORR.Data/Grimoire/Parameters.vb
+++ SPLORR.Data/Grimoire/Parameters.vb
@@ -1,5 +1,4 @@
 ﻿Friend Module Parameters
     Friend Const PARAMETER_CHARACTER_ID = "@CharacterId"
     Friend Const PARAMETER_DIRECTION_ID = "@DirectionId"
-    Friend Const PARAMETER_DIRECTION_NAME = "@DirectionName"
 End Module
