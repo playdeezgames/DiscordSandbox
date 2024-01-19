80a7a0f5c60242ebc8ece51fe94519db1c9d6848
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-01-19 04:57:07 -0600

    added minimum and maximum values

--- SPLORR.Database/dbo/Tables/Character/Type/CharacterTypeStatistics.sql
+++ SPLORR.Database/dbo/Tables/Character/Type/CharacterTypeStatistics.sql
@@ -4,8 +4,12 @@
 	CharacterTypeId INT NOT NULL,
 	StatisticTypeId INT NOT NULL,
 	StatisticValue INT NOT NULL,
-	CONSTRAINT PK_CharacterTypeStatistics PRIMARY KEY (CharacterTypeStatisticId),
+	[MinimumValue] INT NULL, 
+    [MaximumValue] INT NULL, 
+    CONSTRAINT PK_CharacterTypeStatistics PRIMARY KEY (CharacterTypeStatisticId),
 	CONSTRAINT AK_CharacterTypeStatistics_CharacterTypeId_StatisticTypeId UNIQUE(CharacterTypeId, StatisticTypeId),
 	CONSTRAINT FK_CharacterTypeStatistics_CharacterTypes FOREIGN KEY (CharacterTypeId) REFERENCES CharacterTypes(CharacterTypeId),
-	CONSTRAINT FK_CharacterTypeStatistics_StatisticTypes FOREIGN KEY (StatisticTypeId) REFERENCES StatisticTypes(StatisticTypeId)
+	CONSTRAINT FK_CharacterTypeStatistics_StatisticTypes FOREIGN KEY (StatisticTypeId) REFERENCES StatisticTypes(StatisticTypeId),
+	CONSTRAINT CK_CharacterTypeStatistics_StatisticValue CHECK ((MinimumValue IS NULL OR StatisticValue>=MinimumValue) AND (MaximumValue IS NULL OR StatisticValue<=MaximumValue)),
+	CONSTRAINT CK_CharacterTypeStatistics_MaximumValue CHECK (MinimumValue IS NULL OR MaximumValue IS NULL OR MinimumValue <= MaximumValue)
 )
