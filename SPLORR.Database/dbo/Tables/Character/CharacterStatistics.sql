7f7f8ebaacc4f5766b4325214fac40f9150083d0
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-01-19 05:51:42 -0600

    min and max for char stats

--- SPLORR.Database/dbo/Tables/Character/CharacterStatistics.sql
+++ SPLORR.Database/dbo/Tables/Character/CharacterStatistics.sql
@@ -4,8 +4,12 @@
 	CharacterId INT NOT NULL,
 	StatisticTypeId INT NOT NULL,
 	StatisticValue INT NOT NULL,
+	[MinimumValue] INT NULL, 
+    [MaximumValue] INT NULL,
 	CONSTRAINT PK_CharacterStatistics PRIMARY KEY (CharacterStatisticId),
 	CONSTRAINT AK_CharacterStatistics_CharacterId_StatisticTypeId UNIQUE(CharacterId, StatisticTypeId),
 	CONSTRAINT FK_CharacterStatistics_Characters FOREIGN KEY (CharacterId) REFERENCES Characters(CharacterId),
-	CONSTRAINT FK_CharacterStatistics_StatisticTypes FOREIGN KEY (StatisticTypeId) REFERENCES StatisticTypes(StatisticTypeId)
+	CONSTRAINT FK_CharacterStatistics_StatisticTypes FOREIGN KEY (StatisticTypeId) REFERENCES StatisticTypes(StatisticTypeId),
+	CONSTRAINT CK_CharacterStatistics_StatisticValue CHECK ((MinimumValue IS NULL OR StatisticValue>=MinimumValue) AND (MaximumValue IS NULL OR StatisticValue<=MaximumValue)),
+	CONSTRAINT CK_CharacterStatistics_MaximumValue CHECK (MinimumValue IS NULL OR MaximumValue IS NULL OR MinimumValue <= MaximumValue)
 )
