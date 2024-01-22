271efa5b194f50944a008571ecaf727d2a7c9b6d
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-01-22 05:22:39 -0600

    added the delete on discard to the db table

--- SPLORR.Database/dbo/Tables/Card/CardType/CardTypes.sql
+++ SPLORR.Database/dbo/Tables/Card/CardType/CardTypes.sql
@@ -2,6 +2,7 @@
 (
 	[CardTypeId] INT NOT NULL IDENTITY(1,1),
 	[CardTypeName] NVARCHAR(100) NOT NULL,
-	CONSTRAINT PK_CardTypes PRIMARY KEY (CardTypeId),
+	[DeleteOnDiscard] BIT NOT NULL, 
+    CONSTRAINT PK_CardTypes PRIMARY KEY (CardTypeId),
 	CONSTRAINT AK_CardTypes_CardTypeName UNIQUE(CardTypeName)
 );
